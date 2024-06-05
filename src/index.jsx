719b2ea6542869ba107a0f1466b1c9e79bcc13f6
MasaKnd <masahiro.knd.ds@gmail.com>
2024-06-05 11:03:45 -0400

    moved <Canvas> content from index.jsx to App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,11 @@
 import "./style.css";
-import * as THREE from "three";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-const cameraSettings = {
-  fov: 45,
-  near: 0.1,
-  far: 200,
-  position: [5, 2, 10],
-};
-
-const glSettings = {
-  antialias: true,
-  toneMapping: THREE.ACESFilmicToneMapping,
-  outPutColorSpace: THREE.SRGBColorSpace,
-  transparent: true,
-  depthWrite: false,
-};
-
 root.render(
   <>
-    <StrictMode>
-      <Canvas shadows dpr={[1, 2]} camera={cameraSettings} gl={glSettings}>
-        <Experience />
-      </Canvas>
-    </StrictMode>
+    <App />
   </>
 );
