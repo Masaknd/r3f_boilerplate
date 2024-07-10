2ca57f0e2e5ce91ffdce5d65d6bf3a57ccedff06
MasaKnd <masahiro.knd.ds@gmail.com>
2024-07-09 23:31:52 -0400

    added 'React.StrictMode' in index.jsx and deleted <StrictMode> in App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import "./style.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import React from "react";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <>
+  <React.StrictMode>
     <App />
-  </>
+  </React.StrictMode>
 );
